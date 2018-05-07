735e8498d4127fab24ce605555a47bb5001312bf
Blockchain Git <37912779+BlockchainGit@users.noreply.github.com>
2018-05-08 01:35:31 +1200

    Update waihekecoin.sol

--- waihekecoin.sol
+++ waihekecoin.sol
@@ -1,5 +1,6 @@
 pragma solidity ^0.4.23;
 
+
 /**
  * The Locality Coin
  * 
@@ -14,26 +15,27 @@ pragma solidity ^0.4.23;
  * 
  * Safegaurd against overflow errors.
  */
-library OverflowProofArithmetic192 {
-    function add(uint192 a, uint192 b) internal pure returns (uint192 c) {
+library OverflowProofArithmetic96 {
+    function add(uint96 a, uint96 b) internal pure returns (uint96 c) {
         c = a + b;
-        assert(c >= a && c >= b, "Overflow detected in addition");
+        require(c >= a && c >= b, "Overflow detected in addition");
     }
-    function sub(uint192 a, uint192 b) internal pure returns (uint192 c) {
+    function subtract(uint96 a, uint96 b) internal pure returns (uint96 c) {
         require(b <= a, "Subtraction will result in a negative value");
         c = a - b;
-        assert(c <= a, "Overflow detected in subtraction");
+        require(c <= a, "Overflow detected in subtraction");
     }
-    function mul(uint192 a, uint192 b) internal pure returns (uint192 c) {
+    function multiply(uint96 a, uint96 b) internal pure returns (uint96 c) {
         c = a * b;
-        assert(c >= a && c >= b, "Overflow detected in multiplication");
+        require(c >= a && c >= b, "Overflow detected in multiplication");
     }
-    function div(uint192 a, uint192 b) internal pure returns (uint192 c) {
+    function divide(uint96 a, uint96 b) internal pure returns (uint96 c) {
         require(b > 0, "Divide by zero error");
         c = a / b;
-        assert(c <= a, "Overflow detected in division");
+        require(c <= a, "Overflow detected in division");
     }
 }
+
 
 /**
  * ERC Token Standard #20 Interface
@@ -41,8 +43,8 @@ library OverflowProofArithmetic192 {
  * https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20-token-standard.md
  */
 contract ERC20Token {
-    uint public constant totalSupply;
-
+    function totalSupply() public constant
+        returns (uint);
     function balanceOf(address tokenOwner) public constant
         returns (uint balance);
     function allowance(address tokenOwner, address spender) public constant
@@ -67,12 +69,13 @@ contract ERC20Token {
     );
 }
 
+
 contract Owned {
     address public owner;
     address public ownerChangeRequest = 0x0;
 
     modifier onlyOwner() {
-    require(
+        require(
             msg.sender == owner,
             "Only the owner can run this function"
         );
@@ -103,6 +106,7 @@ contract Owned {
         return true;
     }
 }
+
 
 contract Suspendable is Owned {
     /**
@@ -141,40 +145,6 @@ contract Suspendable is Owned {
     }
 }
 
-contract Attributable is Owned {
-    mapping (string => string) public attributes;
-
-    /**
-     * Attribute getter function
-     * 
-     * Get the value of the `_attrName` attribute.
-     * 
-     * @param _attrName The name of the attribute
-     */
-    function getAttribute(string _attrName)
-    view
-    public
-    returns (string attrValue) {
-        return attributes[_attrName];
-    }
-
-    /**
-     * Attribute setter function
-     * 
-     * Set the value of the `_attrName` attribute to `_attrValue`.  This can only
-     * by done by the contract owner.
-     * 
-     * @param _attrName The name of the attribute
-     * @param _attrValue The value of the attribute
-     */
-    function setAttribute(string _attrName, string _attrValue)
-    onlyOwner
-    public
-    returns (bool success) {
-        attributes[_attrName] = _attrValue;
-        return true;
-    }
-}
 
 interface localityCoinRecipient {
     function receiveApproval(
@@ -185,16 +155,27 @@ interface localityCoinRecipient {
     ) external;
 }
 
-contract LocalityCoin is ERC20Token, Owned, Suspendable, Attributable {
-    using OverflowProofArithmetic192 for uint192;
+
+contract LocalityCoin is ERC20Token, Owned, Suspendable {
+    using OverflowProofArithmetic96 for uint96;
 
     string public constant about =
         "The Locality Coin exists to promote provences, cities, towns and villages.  It was created by Warwick Allen (warwick.allen@bchain.expert) on 7 May 2018 and is licenced under the GNU GENERAL PUBLIC LICENSE Version 3.0, which can be viewed at https://www.gnu.org/licenses/gpl-3.0.txt.";
-    uint8 constant public decimals = 18;        // Minimum divisible unit.
-    uint96 constant public totalSupply = -1;    // ~79 billion whole coins.
+    uint8 constant public decimals = 18;
+    uint96 constant public totalWholeUnits   = 7.9228162514e28;
+    uint96 constant public totalMinimumUnits = 7.9228162514e10;
+    uint96 constant internal creatorFee = 514e18;
 
+    string public locality;
+    string public country;
     string public name;
     string public symbol;
+    string public symbolCharacter;
+    string public creator;
+    string public creatorEmail;
+    string public blurb;
+    string public miscellaneous;
+
     mapping (address => uint96) public localityCoinBalance;
     mapping (address => mapping (address => uint96)) public allowance;
 
@@ -211,48 +192,113 @@ contract LocalityCoin is ERC20Token, Owned, Suspendable, Attributable {
      * the contract (i.e., the "owner").
      */
     constructor(
-        string _provence,
+        string _locality,
         string _country,
         string _name,
-        string _symbol
+        string _symbol,
+        string _symbolCharacter,
+        string _creator,
+        string _creatorEmail,
+        string _blurb,
+        string _miscellaneous
     )
     internal
     {
-        attributes['Locality'] = _locality;
-        attributes['Country'] = _country;
-        attributes['Name'] = name = _name;
-        attributes['Symbol'] = symbol = _symbol;
+        locality = _locality;
+        country = _country;
+        name = _name;
+        symbol = _symbol;
+        symbolCharacter = _symbolCharacter;
+        creator = _creator;
+        creatorEmail = _creatorEmail;
+        blurb = _blurb;
+        miscellaneous = _miscellaneous;
 
         owner = msg.sender;
-        localityCoinBalance[this] = totalSupply;
+        localityCoinBalance[owner] = creatorFee;
+        localityCoinBalance[this] = totalMinimumUnits.subtract(creatorFee);
     }
 
     /**
-     * Internal transfer, only can be called by this contract
+     * Fall-back function to receive ether
      */
-    function _transfer(address _from, address _to, uint _value)
-    internal
-    {
-        require(_to != 0x0, "Cannot transfer to 0x0");
-        require(
-            localityCoinBalance[_from] >= _value,
-            "The sender's localityCoin balance is too low"
-        );
-        require(
-            localityCoinBalance[_to] + _value >= localityCoinBalance[_to],
-            "Overflow detected"
-        );
+    function()
+    public
+    payable
+    { }
 
-        uint256 sumOfStartingBalances =
-            localityCoinBalance[_from] + localityCoinBalance[_to];
-        localityCoinBalance[_from] -= _value;   // Subtract from the sender
-        localityCoinBalance[_to] += _value;     // Add the same to the recipient
-        // Sanity check.
-        assert(
-            localityCoinBalance[_from] + localityCoinBalance[_to] ==
-                sumOfStartingBalances
-        );
-        emit Transfer(_from, _to, _value);
+    /**
+     * Getter functions
+     */
+    function getLocality() view public returns (string) {
+        return locality;
+    }
+    function getCountry() view public returns (string) {
+        return country;
+    }
+    function getName() view public returns (string) {
+        return name;
+    }
+    function getSymbol() view public returns (string) {
+        return symbol;
+    }
+    function getSymbolCharacter() view public returns (string) {
+        return symbolCharacter;
+    }
+    function getCreator() view public returns (string) {
+        return creator;
+    }
+    function getCreatorEmail() view public returns (string) {
+        return creatorEmail;
+    }
+    function getBlurb() view public returns (string) {
+        return blurb;
+    }
+    function getMiscellaneous() view public returns (string) {
+        return miscellaneous;
+    }
+
+    /**
+     * Setter functions
+     */
+    function setLocality(string newValue) onlyOwner public returns (string) {
+        return locality = newValue;
+    }
+    function setCountry(string newValue) onlyOwner public returns (string) {
+        return country = newValue;
+    }
+    function setName(string newValue) onlyOwner public returns (string) {
+        return name = newValue;
+    }
+    function setSymbol(string newValue) onlyOwner public returns (string) {
+        return symbol = newValue;
+    }
+    function setSymbolCharacter(string newValue) onlyOwner public returns (string) {
+        return symbolCharacter = newValue;
+    }
+    function setCreator(string newValue) onlyOwner public returns (string) {
+        return creator = newValue;
+    }
+    function setCreatorEmail(string newValue) onlyOwner public returns (string) {
+        return creatorEmail = newValue;
+    }
+    function setBlurb(string newValue) onlyOwner public returns (string) {
+        return blurb = newValue;
+    }
+    function setMiscellaneous(string newValue) onlyOwner public returns (string) {
+        return miscellaneous = newValue;
+    }
+
+    /**
+     * Total Supply
+     * 
+     * The total number of whole coins.
+     */
+    function totalSupply()
+    view
+    public
+    returns (uint supply) {
+        return totalWholeUnits;
     }
 
     /**
@@ -266,9 +312,9 @@ contract LocalityCoin is ERC20Token, Owned, Suspendable, Attributable {
     function transfer(address _to, uint256 _volume)
     onlyWhenActive
     public
-    returns (uint256 volume) {
-        _transfer(msg.sender, _to, _volume);
-        return _volume;
+    returns (bool success) {
+        _transfer(msg.sender, _to, uint96(_volume));
+        return true;
     }
 
     /**
@@ -283,14 +329,16 @@ contract LocalityCoin is ERC20Token, Owned, Suspendable, Attributable {
     function transferFrom(address _from, address _to, uint256 _volume)
     onlyWhenActive
     public
-    returns (uint192 volume) {
+    returns (bool success) {
+        uint96 thisAllowance = allowance[_from][msg.sender];
+        uint96 volume = uint96(_volume);
         require(
-            _volume <= allowance[_from][msg.sender],
+            volume <= thisAllowance,
             "The allowance is insufficient for this transfer"
         );
-        allowance[_from][msg.sender] -= _volume;
-        _transfer(_from, _to, _volume);
-        return _volume;
+        allowance[_from][msg.sender] = thisAllowance.subtract(volume);
+        _transfer(_from, _to, volume);
+        return true;
     }
 
     /**
@@ -302,12 +350,13 @@ contract LocalityCoin is ERC20Token, Owned, Suspendable, Attributable {
      * @param _spender The address authorized to spend
      * @param _volume the max amount they can spend
      */
-    function approve(address _spender, uint192 _volume)
+    function approve(address _spender, uint _volume)
     onlyWhenActive
     public
     returns (bool success) {
-        allowance[msg.sender][_spender] = _value;
-        emit Approval(msg.sender, _spender, _value);
+        uint96 volume = uint96(_volume);
+        allowance[msg.sender][_spender] = volume;
+        emit Approval(msg.sender, _spender, volume);
         return true;
     }
 
@@ -321,7 +370,7 @@ contract LocalityCoin is ERC20Token, Owned, Suspendable, Attributable {
      * @param _volume The max amount they can spend
      * @param _extraData Some extra information to send to the approved contract
      */
-    function approveAndCall(address _spender, uint192 _volume, bytes _extraData)
+    function approveAndCall(address _spender, uint96 _volume, bytes _extraData)
     onlyWhenActive
     public
     returns (bool success) {
@@ -340,12 +389,12 @@ contract LocalityCoin is ERC20Token, Owned, Suspendable, Attributable {
      * @param _from the address of the sender
      * @param _volume the amount of localitycoin to revert
      */
-    function revertToContractFrom(address _from, uint192 _volume)
+    function revertToContractFrom(address _from, uint96 _volume)
     onlyWhenActive
     public
     returns (bool success) {
-        _transfer(_from, this, _value);
-        emit RevertToContract(_from, _value);
+        _transfer(_from, this, _volume);
+        emit RevertToContract(_from, _volume);
         return true;
     }
 
@@ -356,7 +405,7 @@ contract LocalityCoin is ERC20Token, Owned, Suspendable, Attributable {
      *
      * @param _volume the amount of money to revert
      */
-    function revertToContract(uint192 _volume)
+    function revertToContract(uint96 _volume)
     onlyWhenActive
     public
     returns (bool success) {
@@ -371,7 +420,7 @@ contract LocalityCoin is ERC20Token, Owned, Suspendable, Attributable {
     function balanceOf()
     view
     public
-    returns (uint256) {
+    returns (uint) {
         return localityCoinBalance[msg.sender];
     }
 
@@ -383,10 +432,10 @@ contract LocalityCoin is ERC20Token, Owned, Suspendable, Attributable {
     function balanceOfContract()
     view
     public
-    returns (uint192 volume) {
+    returns (uint volume) {
         return localityCoinBalance[this];
     }
-    
+
     /**
      * Get the price of localitycoin in ether
      */
@@ -394,7 +443,7 @@ contract LocalityCoin is ERC20Token, Owned, Suspendable, Attributable {
     view
     public
     returns (uint96 price) {
-        return (totalSupply - localityCoinBalance[this]) >> 2;
+        return (totalMinimumUnits.subtract(localityCoinBalance[this])) >> 2;
     }
 
     /**
@@ -408,8 +457,9 @@ contract LocalityCoin is ERC20Token, Owned, Suspendable, Attributable {
     function getValue(uint96 _volume)
     view
     public
-    returns (uint192 value) {
-        return uint192(getPrice()) * _volume;
+    returns (uint96 value) {
+        uint96 price = getPrice();
+        return price.multiply(_volume);
     }
 
     /**
@@ -420,11 +470,12 @@ contract LocalityCoin is ERC20Token, Owned, Suspendable, Attributable {
      * 
      * @param _value The amount of wei
      */
-    function getBuyVolume(uint192 _value)
+    function getBuyVolume(uint96 _value)
     view
     public
     returns (uint96 volume) {
-        return _value / getPrice();
+        uint96 price = getPrice();
+        return _value.divide(price);
     }
 
     /**
@@ -436,8 +487,13 @@ contract LocalityCoin is ERC20Token, Owned, Suspendable, Attributable {
     payable
     onlyWhenActive
     public
-    returns (uint192 volume) {
-        volume = getBuyVolume(msg.amount);
+    returns (uint96 volume) {
+        uint96 value = uint96(msg.value);
+        require(
+            uint256(value) == msg.value,
+            "Overflow detected in the amount of paid ether"
+        );
+        volume = getBuyVolume(value);
         _transfer(this, msg.sender, volume); // from contract to seller
         return volume;
     }
@@ -447,10 +503,10 @@ contract LocalityCoin is ERC20Token, Owned, Suspendable, Attributable {
      * 
      * Redeem localitycoin for ether.
      */
-    function sell(uint192 _amount)
+    function sell(uint96 _amount)
     onlyWhenActive
     public
-    returns (uint192 value){
+    returns (uint96 value){
         _transfer(msg.sender, this, _amount); // from seller to contract
         value = getValue(_amount);
         // Send ether to the seller.  It's important to do this last to prevent
@@ -460,23 +516,38 @@ contract LocalityCoin is ERC20Token, Owned, Suspendable, Attributable {
     }
 
     /**
-     * Fall-back function to receive ether
+     * Internal transfer, only can be called by this contract
      */
-    function()
-    public
-    payable
-    { }
+    function _transfer(address _from, address _to, uint96 _value)
+    internal
+    {
+        require(_to != 0x0, "Cannot transfer to 0x0");
+        require(
+            localityCoinBalance[_from] >= _value,
+            "The sender's localityCoin balance is too low"
+        );
+
+        localityCoinBalance[_from] = localityCoinBalance[_from].subtract(
+            _value
+        );   // Subtract from the sender
+
+        localityCoinBalance[_to] = localityCoinBalance[_to].add(
+            _value
+        );     // Add the same to the recipient
+
+        emit Transfer(_from, _to, _value);
+    }
 }
+
 
 contract WaihekeCoin is LocalityCoin(
     "Waiheke Island",
     "New Zealand",
     "Waiheke Coin",
-    "WAIHK"
-) {
-    constructor() {
-        setAttribute('Symbol Character', '₩');
-        setAttribute('Creator', 'Warwick Allen');
-        setAttribute('Creator Email', '<warwick.allen@blockchain.expert>');
-    }
-}
+    'WAIHK',
+    '₩',
+    'Warwick Allen',
+    '<warwick.allen@blockchain.expert>',
+    '',
+    ''
+) { constructor() internal { } }
